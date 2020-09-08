7dccd587c3779ee44e4e5077e413061f8c5ddb04
Olga Kuznetsova <olya.kuznetsova.2015@list.ru>
2020-09-08 22:09:49 +0300

    Updates

---
+++ precache-manifest.e1498353a02967b277b653765f9abc7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5ca513fed7f2a7f7a47152d2202c5b",
+    "url": "/project.github.io/index.html"
+  },
+  {
+    "revision": "5e6a4bc52da002081dbe",
+    "url": "/project.github.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a5c22add2b89d9896059",
+    "url": "/project.github.io/static/js/2.b5471947.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/project.github.io/static/js/2.b5471947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6a4bc52da002081dbe",
+    "url": "/project.github.io/static/js/main.540462bb.chunk.js"
+  },
+  {
+    "revision": "994326cd37fe1119f624",
+    "url": "/project.github.io/static/js/runtime-main.e6be68b9.js"
+  }
+]);
